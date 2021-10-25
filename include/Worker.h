8d5c070dc8f32f4a6bd8c73b4cec94c5143d208d
woniuhuhu <jh18205234513@163.com>
2021-10-25 18:49:36 +0800

    ...

--- include/Worker.h
+++ include/Worker.h
@@ -1,10 +1,13 @@
 #pragma once
 #include <thread>
-class Sunnet;
+//#include "Sunnet.h"
+//class Sunnet;
 using namespace std;
+
+
 class Worker {
     public:
         int id;//编号
         int eachNum;//每次处理多少条消息
         void operator()();//线程函数
-}
+};
